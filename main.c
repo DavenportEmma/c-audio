a3bd827d32fcebc0f8e4d78f58af64dc19689092
Conor Davenport <conor.davenport@microchip.com>
2023-08-28 11:42:40 +0100

    initial commit
    Signed-off-by: Conor Davenport <conor.davenport@microchip.com>

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    typedef struct {
+        char file_type[4]; // RIFF
+        int file_size;
+        char file_type_header[4]; // WAVE
+        char format_chunk_marker[4]; // fmt
+        int format_data_len; // length of format data as listed above
+        short int format_type; // 1
+        short int num_channels; // 2
+        int sample_rate; // 44100
+        int byte_rate; // 176400
+        short int total_bytes_per_sample; // 4
+        short int bits_per_sample; // 16
+        char data_start[4]; // data
+        int data_len; // size of the data section
+    } header;
+
+    header h = {
+        "RIFF",
+        36,
+        "WAVE",
+        "fmt\x20",
+        16,
+        1,
+        2,
+        44100,
+        176400,
+        4,
+        16,
+        "data",
+        0
+    };
+
+    FILE *fp;
+    fp = fopen("a.wav", "w");
+
+    if (fp == NULL) {
+        fprintf(stderr, "\nError opened file\n");
+        exit(1);
+    }
+
+    int flag = 0;
+    flag = fwrite(&h, sizeof(header), 1, fp);
+
+
+    if (flag) {
+        printf("Contents of the structure written "
+               "successfully");
+    }
+    else
+        printf("Error Writing to File!");
+ 
+    // close file
+    fclose(fp);
+
+    return 0;
+}
