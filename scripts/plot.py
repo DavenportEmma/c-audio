cfa58d648c52fb884fe26ee23db837885bad722f
Conor Davenport <conor.davenport@microchip.com>
2023-10-03 10:45:06 +0100

    plot: read data from file

--- scripts/plot.py
+++ scripts/plot.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 
 def main():
     data = []
+    with open('out/a.out.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            data.append(line)
 
     plt.plot(data)
+    plt.plot(data, "g*")
     plt.show()
 
 if __name__ == "__main__":
