bd12365ef3c510a7bf96181915ceb2bf76ff409c
ConorDavenport <cnrdavenport@gmail.com>
2023-11-19 20:58:37 +0000

    fix conflict

--- src/filter.c
+++ src/filter.c
@@ -2,39 +2,33 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "filter.h"
 
 void filter(
-    FilterDescriptor f,
-    int16_t* d,
-    int16_t* o,
-    int len
+    double a[],
+    int a_len,
+    double b[],
+    int b_len,
+    int16_t* x,
+    int x_len,
+    int16_t* filtered
 ) {
-    double* temp = (double*)calloc(len, len * sizeof(double));
+    for(int i = 0; i < x_len; i++) {
+        double a1, a2, b1, b2, b3 = 0;
+        if(i >= 1) {
+            a1 = a[1]*x[i-1];
+            b1 = b[1]*filtered[i-1];
+        }
 
-    for(int i=0; i<len; i++) {
-        int xn = d[i];
-        int xn1 = (i >= 1 ? d[i-1] : 0);
-        int xn2 = (i >= 2 ? d[i-2] : 0);
-        int xn3 = (i >= 3 ? d[i-3] : 0);
+        if(i >= 2) {
+            a2 = a[2]*x[i-2];
+            b2 = b[2]*filtered[i-2];
+        }
 
-        double tn1 = (i >= 1 ? temp[i-1] : 0);
-        double tn2 = (i >= 2 ? temp[i-2] : 0);
-        double tn3 = (i >= 3 ? temp[i-3] : 0);
+        if(i >= 3) {
+            b3 = b[3]*filtered[i-3];
+        }
 
-        double a0 = f.a[0];
-        double a1 = f.a[1];
-        double a2 = f.a[2];
-        double a3 = f.a[3];
 
-        double b0 = f.b[0];
-        double b1 = f.b[1];
-        double b2 = f.b[2];
-        double b3 = f.b[3];
-
-        temp[i] = xn*a0 + xn1*a1 + xn2*a2 + xn3*a3 - tn1*b1 - tn2*b2 - tn3*b3;
-        o[i] = (int16_t)temp[i];
+        filtered[i] = a[0]*x[0] + a1 + a2 - b1 - b2 - b3;
     }
-
-    free(temp);
 }
