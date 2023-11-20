7bbc9b17d2f121740b79dd097123d35d32190aeb
ConorDavenport <cnrdavenport@gmail.com>
2023-11-20 23:16:07 +0000

    function for reading data from wav

---
+++ include/readWav.h
@@ -0,0 +1,19 @@
+#ifndef READ_WAV_H
+#define READ_WAV_H
+
+#include <stdint.h>
+#include "wav_header.h"
+
+int getMetadata(
+    char* file_name,
+    WavHeader* h
+);
+
+int extractAudioData(
+    char* file_name,
+    WavHeader* h,
+    int16_t* buffer,
+    int buffer_len
+);
+
+#endif
--- src/readWav.c
+++ src/readWav.c
@@ -1,3 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "readWav.h"
+#include "wav_header.h"
+
+int getMetadata(char* file_name, WavHeader* h) {
+    FILE *fp;
+    fp = fopen("audio/AM.wav", "rb");
+
+    if(fp == NULL) {
+        fprintf(stderr, "\nError opening file\n");
+        exit(1);
+    }
+
+    fread(h, sizeof(WavHeader), 1, fp);
+    return 0;
+}
+
+int extractAudioData(
+    char* file_name, WavHeader* h, int16_t* buffer, int buffer_len
+) {
+    FILE *fp;
+    fp = fopen("audio/AM.wav", "rb");
+
+    if(fp == NULL) {
+        fprintf(stderr, "\nError opening file\n");
+        exit(1);
+    }
+
+    fread(h, sizeof(WavHeader), 1, fp);
+    fread(buffer, sizeof(int16_t), buffer_len, fp);
+
+    return 0;
+}
